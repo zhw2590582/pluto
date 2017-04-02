1ed74f703fe04074c85e6cf97c0eaaae238491c0
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-04-02 10:10:26 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -133,13 +133,13 @@ $('body').on('click', '#comment-nav-below a', function(e) {
 //通知Modal
 	$(".notice-close").click(function() {
 		$(".notice").hide();
-		setCookie("notice","close","h1");
+    sessionStorage.setItem("notice", "hide");
 	});
 
-	if(!getCookie("notice")){
+	if(!!sessionStorage.getItem("notice")){
+    $(".notice").remove();
+	} else {
     $(".notice").addClass("open");
-	}else {
-	  $(".notice").remove();
 	}
 
   //自适应菜单
@@ -159,45 +159,6 @@ $('body').on('click', '#comment-nav-below a', function(e) {
 
 });
 
-//设置cookies
-function setCookie(name,value,time) {
-	var strsec = getsec(time);
-	var exp = new Date();
-	exp.setTime(exp.getTime() + strsec*1);
-	document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
-}
-function getsec(str) {
-	var str1=str.substring(1,str.length)*1;
-	var str2=str.substring(0,1);
-	if (str2=="s") {
-		return str1*1000;
-	} else if (str2=="h") {
-		return str1*60*60*1000;
-	} else if (str2=="d") {
-		return str1*24*60*60*1000;
-	}
-}
-//s20是代表20秒
-//h是指小时，如12小时则是：h12
-//d是天数，30天则：d30
-
-//读取cookies
-function getCookie(name) {
-	var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-	if(arr=document.cookie.match(reg))
-		return unescape(arr[2]);
-	else
-		return null;
-}
-
-//删除cookies
-function delCookie(name) {
-	var exp = new Date();
-	exp.setTime(exp.getTime() - 1);
-	var cval=getCookie(name);
-	if(cval!=null)
-		document.cookie= name + "="+cval+";expires="+exp.toGMTString();
-}
 
 // 图像懒加载
 echo.init({
